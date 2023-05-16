d95070a1567dcd1d09a2ff923d4a75ac8614870a
Eyder Rios <eyder.rios@gmail.com>
2023-05-16 08:50:14 -0300

    Number

--- number/bin/main.dart
+++ number/bin/main.dart
@@ -2,7 +2,7 @@ import 'number.dart';
 
 void main() {
   Number z = Number();
-  Number n1 = Number(9473);
+  Number n1 = Number(653);
 
   print('z  = $z');
   print('n1 = $n1');
@@ -10,4 +10,9 @@ void main() {
   print('z.zero(): ${z.zero}');
   print('n1.prime(): ${n1.prime}');
   print('n1.narc(): ${n1.narcissistic}');
+  print('n1!: ${n1.factorial}');
+  print('sqrt(n1): ${n1.sqrt}');
+  print('n1.digitsSum(10): ${n1.digitsSum()}');
+  print('n1.reverse: ${n1.reverse}');
+  print('n1.toBase(2): ${n1.toBase(16, false)}');
 }
--- number/bin/number.dart
+++ number/bin/number.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 class Number {
   final int _number;
@@ -32,7 +32,7 @@ class Number {
     int number = _number;
 
     while (number != 0) {
-      sum += pow(number % 10, digits).toInt();
+      sum += math.pow(number % 10, digits).toInt();
       number ~/= 10;
     }
 
@@ -49,5 +49,60 @@ class Number {
     }
 
     return (_number != 0) ? digits : 1;
+  }
+
+  int get factorial {
+    int result = 1;
+    for (var k = 2; k <= _number; k++) {
+      result *= k;
+    }
+    return result;
+  }
+
+  double get sqrt => math.sqrt(_number);
+  int get sqr => _number * _number;
+
+  int digitsSum([int base = 10]) {
+    int sum = 0;
+    int number = _number;
+
+    if (base < 2) {
+      throw Exception('Invalid base: $base');
+    }
+
+    while (number != 0) {
+      sum += number % base;
+      number = number ~/ base;
+    }
+    return sum;
+  }
+
+  int get reverse {
+    int sum = 0;
+    int number = _number;
+
+    while (number != 0) {
+      sum = sum * 10 + (number % 10);
+      number ~/= 10;
+    }
+
+    return sum;
+  }
+
+  String toBase(int base, [bool lowerCase = true]) {
+    String result = '';
+    int number = _number;
+
+    while (number != 0) {
+      int digit = number % base;
+      if (digit < 10) {
+        result = digit.toString() + result;
+      } else {
+        digit = lowerCase ? digit + 87 : digit + 55;
+        result = String.fromCharCode(digit) + result;
+      }
+      number ~/= base;
+    }
+    return result;
   }
 }
