300c1020853eadec89c7330f724dd1482d332508
Eyder Rios <eyder.rios@gmail.com>
2023-06-26 08:10:55 -0300

    2a Avaliação - Arquivos

---
+++ aval2/bin/main.dart
@@ -0,0 +1,38 @@
+//
+// ESTE ARQUIVO NÃO DEVE SER ALTERADO
+//
+
+import 'package:diglib/digital_library.dart';
+import 'package:diglib/media.dart';
+
+void main() {
+  const mediaFileName = 'data/media.json';
+
+  final lib = DigitalLibrary();
+  int totalDuration;
+
+  lib.loadMedia(mediaFileName);
+
+  print('#### FILMES\n');
+  totalDuration = lib.totalMediaDuration(MediaType.movie);
+  lib.listMedia(MediaType.movie);
+  print('DURAÇÂO TOTAL: $totalDuration min\n');
+
+  print('#### LIVROS\n');
+  totalDuration = lib.totalMediaDuration(MediaType.book);
+  lib.listMedia(MediaType.movie);
+  print('DURAÇÂO TOTAL: $totalDuration min\n');
+
+  print('#### MÙSICAS\n');
+  totalDuration = lib.totalMediaDuration(MediaType.music);
+  lib.listMedia(MediaType.movie);
+  print('DURAÇÂO TOTAL: $totalDuration min\n');
+
+  print('#### TODAS AS MÍDIAS\n');
+  totalDuration = lib.totalMediaDuration();
+  lib.listMedia();
+  print('DURAÇÂO TOTAL: $totalDuration min\n');
+
+  // ESTA LINHA PODE SER ALTERADA
+  print('AUTORES: Fulano e Cicrano');
+}
